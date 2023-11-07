7391999786c4e1b7fe9e5c4615c413f2485a5b00
רועי סביון <roysav123@gmail.com>
2023-11-07 15:56:34 +0200

    created a get_listings function

--- src/autoscout24/__init__.py
+++ src/autoscout24/__init__.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import Optional
 
 import requests
@@ -9,13 +10,13 @@ from .utils import _url_repr
 _BASE_URL = 'https://autoscout24.com'
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(slots=True, frozen=True)
 class Maker:
     id: str
     name: str
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(slots=True, frozen=True)
 class Model:
     id: int
     name: str
@@ -62,5 +63,45 @@ def get_makers() -> list[Maker]:
     ]
 
 
+def _listings_url(maker: Maker, model: Model, **options):
+    return (
+        f'{_BASE_URL}/lst/'
+        f'{_url_repr(maker.name)}/'
+        f'{_url_repr(model.name)}?'
+        f'{urllib.parse.urlencode(options)}'
+    )
 
 
+def _parse_data_dict(article_attrs: dict[str, str]):
+    """
+    :param article_attrs: dict of the <article/> element attributes .i.e {"class": "class1 class2", "data-maker": "bmw"}
+    :return: only keys starting with "data-" and rename "data-*" -> "*"
+        .i.e the key "data-maker" will be renamed as just "maker"
+    """
+    data = {}
+    for key, value in article_attrs.items():
+        if key.startswith('data-'):
+            new_key = key.replace('data-', '', 1)
+            data[new_key] = value
+    return data
+
+
+def _parse_listing_page(page_html: str) -> list[dict]:
+    """
+    :param page_html: html of the entire page
+    :return: a `ListingsPage` object
+    """
+    selector = parsel.Selector(page_html)
+    listings_data = []
+    for article in selector.css('article'):
+        data = _parse_data_dict(article.attrib)
+        listings_data.append(data)
+    return listings_data
+
+
+def get_listings(maker: Maker, model: Model, **options):
+    response = requests.get(_listings_url(maker, model, **options))
+    response.raise_for_status()
+
+    page_html = response.text
+    return _parse_listing_page(page_html)
---
+++ tests/main.py
@@ -0,0 +1,10 @@
+from src import autoscout24
+import pandas as pd
+
+
+maker = autoscout24.Maker(id='9', name='audi')
+model = autoscout24.Model(id=16416, name='A2')
+
+listings = autoscout24.get_listings(maker, model)
+df = pd.DataFrame(listings)
+print(df.columns)
